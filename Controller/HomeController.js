00f14d5e4af32c9f96f7ce1f1470c164b8a0b460
Lucas Fernandez <lucas.fernandez@daux.com.ar>
2024-03-16 19:45:48 -0300

    Base proyecto Web

---
+++ Controller/HomeController.js
@@ -0,0 +1,82 @@
+var globalIndex = 0;
+
+$(document).ready(function() {
+    GetProducts().then(function(products) {
+        // Almacena los productos en Local Storage
+        localStorage.setItem('Productos', JSON.stringify(products));
+
+        showProducts(products);
+
+    }).catch(function(error) {
+        console.error('Hubo un problema con la solicitud:', error);
+        // Aquí puedes manejar el error si es necesario
+    });
+    
+});
+
+$(window).on("scroll", function() {
+    // Verifica si se ha llegado al final de la página y carga más productos
+    if ($(window).scrollTop() + $(window).height() == $(document).height()) {
+        MoreProducts();
+    }
+});
+
+function showProducts(products) {
+    // Muestra los primeros 4 productos
+    for (var i = globalIndex; i < globalIndex + 4 && i < products.length; i++) {
+        var product = products[i];
+        var html = '<div class="col mb-5">' +
+                        '<div class="card h-100">' +
+                            '<img class="card-img-top" src="' + product.image + '" alt="..." />' +
+                                '<div class="card-body p-4">' +
+                                            '<div class="text-center">' +
+                                                '<h5 class="fw-bolder">' + product.title + '</h5>' +
+                                                product.price +
+                                            '</div>' +
+                                        '</div>' +
+                                    '<div class="card-footer p-4 pt-0 border-top-0 bg-transparent">' +
+                                '<div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Detalles del Producto</a></div>' +
+                            '</div>' +
+                        '</div>' +
+                    '</div>';
+
+        $('#procduct-secction').append(html);
+    }
+    globalIndex += 4; // Incrementa globalIndex después de mostrar los productos
+}
+
+function MoreProducts(){
+    var savedProducts = JSON.parse(localStorage.getItem('Productos'));
+    var remainingProducts = savedProducts.length - globalIndex;
+
+    // Muestra hasta 4 productos adicionales
+    for (var i = globalIndex; i < globalIndex + Math.min(4, remainingProducts); i++) {
+        var product = savedProducts[i];
+        var html = '<div class="col mb-5">' +
+                        '<div class="card h-100">' +
+                            '<img class="card-img-top" src="' + product.image + '" alt="..." />' +
+                                '<div class="card-body p-4">' +
+                                        '<div class="text-center">' +
+                                            '<h5 class="fw-bolder">' + product.title + '</h5>' +
+                                            product.price +
+                                            '</div>' +
+                                        '</div>' +
+                                    '<div class="card-footer p-4 pt-0 border-top-0 bg-transparent">' +
+                                '<div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Detalles del Producto</a></div>' +
+                            '</div>' +
+                        '</div>' +
+                    '</div>';
+
+        $('#procduct-secction').append(html);
+    }
+    globalIndex += 4; // Incrementa globalIndex después de mostrar los productos
+}
+
+function GetProducts(){
+    return fetch('https://fakestoreapi.com/products')
+            .then(res => res.json())
+            .catch(error => {
+            console.error('Hubo un problema con la solicitud:', error);
+            return "Servicio no disponible.";
+            });
+}
---
+++ Model/HomeModel.js
@@ -0,0 +1,16 @@
+// export class ProductsModel{
+//     constructor(Id, Title,Category,Description,Image,Price){
+//         this.Id = Id
+//         this.Title = Title;
+//         this.Category = Category;
+//         this.Description = Description;
+//         this.Image = Image;
+//         this.Price = Price;
+//         Rating = new Rating();
+//     }
+// }
+
+// class Rating {
+//     Count;
+//     Rate;
+// }
